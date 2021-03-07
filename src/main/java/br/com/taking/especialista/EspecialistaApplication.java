25a7a493a33bfae6e065509e009dc9674e10f15f
ROBSON4CTH3 <80186355+ROBSON4CTH3@users.noreply.github.com>
2021-03-07 00:56:44 -0300

    Init

---
+++ src/main/java/br/com/taking/especialista/EspecialistaApplication.java
@@ -0,0 +1,13 @@
+package br.com.taking.especialista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspecialistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspecialistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/taking/especialista/EspecialistaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taking.especialista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspecialistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
